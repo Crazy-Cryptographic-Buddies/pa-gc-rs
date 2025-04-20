925a39f35a968443cae647b64aee773684ca8be4
khaihanhtang <84257457+khaihanhtang@users.noreply.github.com>
2025-04-20 23:47:13 +0800

    Update functionality_pre.rs

--- functionality_pre_insecure/functionality_pre.rs
+++ functionality_pre_insecure/functionality_pre.rs
@@ -1,9 +1,16 @@
 use std::ops::BitXor;
+use rand::Rng;
 use crate::gf::{Random, gf256::GF256};
 
 struct FunctionalityPre<GF: BitXor + Random> {
     delta_a: GF,
     delta_b: GF,
+    s_vec: Vec<u8>,
+    vole_mac_s_vec: Vec<GF>,
+    vole_key_s_vec: Vec<GF>,
+    r_vec: Vec<u8>,
+    vole_mac_r_vec: Vec<GF>,
+    vole_key_r_vec: Vec<GF>,
 }
 
 impl<GF: BitXor + Random> FunctionalityPre<GF> {
@@ -19,6 +26,21 @@ impl<GF: BitXor + Random> FunctionalityPre<GF> {
     }
     pub fn get_delta_b(&self) -> &GF {
         &self.delta_b
+    }
+
+    pub fn generate(&mut self, num_tuples: usize) {
+        let mut rng = rand::rng();
+        self.s_vec = Vec::new();
+        self.r_vec = Vec::new();
+        self.vole_mac_s_vec = Vec::new();
+        self.vole_key_s_vec = Vec::new();
+        self.vole_mac_r_vec = Vec::new();
+        self.vole_key_r_vec = Vec::new();
+        for _ in 0..num_tuples {
+            let s = rng.random::<u8>() & 1;
+            let r = rng.random::<u8>() & 1;
+            // TODO: continue here
+        }
     }
 }
 
